bc7d27b45ed1b9c6dc4b91a186d615c559ff05b1
lightway82 <lightway82@gmail.com>
2017-06-22 15:15:30 +0300

    starter главное окно

--- starter/src/main/java/ru/biomedis/starter/AppController.java
+++ starter/src/main/java/ru/biomedis/starter/AppController.java
@@ -1,10 +1,28 @@
 package ru.biomedis.starter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.web.WebView;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class AppController extends BaseController {
+
+    @FXML private Button installUpdatesBtn;
+    @FXML private Button startProgram;
+    @FXML private WebView webContent;
+    @FXML private ProgressIndicator versionCheckIndicator;
+    @FXML private Label textInfo;
+    @FXML private ProgressBar updateIndicator;
+    @FXML private Hyperlink linkMain;
+    @FXML private Hyperlink linkArticles;
+    @FXML private Hyperlink linkForum;
+    @FXML private Hyperlink linkVideo;
+    @FXML private Hyperlink linkEducation;
+    @FXML private Hyperlink linkVideoM;
+
     @Override
     public void setParams(Object... params) {
 
@@ -12,6 +30,15 @@ public class AppController extends BaseController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        webContent.getEngine().load("http://biomedis.ru");
+    }
+
+
+    public void onInstallUpdates(){
+
+    }
+
+    public void onStartProgram(){
 
     }
 }
