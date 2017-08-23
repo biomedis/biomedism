f147d670d65986d7fd5a1981d6725b5f24dbb274
lightway82 <lightway82@gmail.com>
2017-08-23 12:06:35 +0300

    testing update server

---
+++ update_test_server/src/main/java/ru/biomedis/updateserver/Server.java
@@ -0,0 +1,43 @@
+package ru.biomedis.updateserver;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+
+public class Server extends AbstractVerticle {
+
+
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+
+        Router router = Router.router(vertx);
+        // Serve the static pages
+        router.route().handler(StaticHandler.create());
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(router::accept).listen(port, res -> {
+            if (!res.failed()) {
+                System.out.println("Server is started at port = "+port);
+                startFuture.complete();
+            } else startFuture.failed();
+        });
+
+
+    }
+
+
+
+
+}
---
+++ update_test_server/src/main/java/ru/biomedis/updateserver/TestingUpdateServer.java
@@ -0,0 +1,48 @@
+package ru.biomedis.updateserver;
+
+
+import io.vertx.core.Vertx;
+
+public class TestingUpdateServer {
+    private static boolean started = false;
+    private static Vertx vertx;
+    private static Server server;
+
+    /**
+     * Запуск сервера если еще не запущен
+     */
+    synchronized public static void startServer() {
+        if (started) return;
+
+        System.out.println("Server is starting...");
+        System.setProperty("vertx.disableFileCaching", "true");
+        vertx = Vertx.vertx();
+        server = new Server(3000);
+        vertx.deployVerticle(server, res -> {
+            if (!res.failed()) {
+                started = true;
+                System.out.println(server);
+            } else throw new RuntimeException("Server not running");
+
+        });
+
+        while (started==false){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int getPort() {
+        return server.getPort();
+    }
+
+
+    public static void main(String[] args) {
+
+       startServer();
+
+    }
+}
