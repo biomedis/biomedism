f64e822e86352006186348d50b5d06350b008b94
lightway82 <lightway82@gmail.com>
2017-11-03 16:38:35 +0300

    Правки шаблонов Доработка окна печати

--- src/main/java/ru/biomedis/biomedismair3/Dialogs/PrintController.java
+++ src/main/java/ru/biomedis/biomedismair3/Dialogs/PrintController.java
@@ -4,6 +4,9 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.print.PrinterJob;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Modality;
@@ -16,7 +19,6 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-
 import static ru.biomedis.biomedismair3.Log.logger;
 
 /**
@@ -24,18 +26,22 @@ import static ru.biomedis.biomedismair3.Log.logger;
  */
 public class PrintController extends BaseController {
 
-    private
-    @FXML
-    WebView webView;
+    private   @FXML  ChoiceBox<Float> fontSize;
+    private   @FXML RadioButton freqRadio;
+    private   @FXML RadioButton noFreqRadio;
+
+    private  @FXML WebView webView;
     private WebEngine webEngine;
-    private
-    @FXML
-    Node root;
+    private    @FXML  Node root;
+
+
 
     private Long id;
     private int type;
     private List<Long> ids;
     private ResourceBundle res;
+
+    private static int DEFAULT_FONT_SIZE=10;
 
     @Override
     protected void onCompletedInitialise() {
@@ -65,30 +71,40 @@ public class PrintController extends BaseController {
         res = resources;
         webEngine = webView.getEngine();
 
-        Platform.runLater(() -> webEngine.loadContent(getContent()));
+        ToggleGroup toggleGroup =new ToggleGroup();
+        freqRadio.setToggleGroup(toggleGroup);
+        noFreqRadio.setToggleGroup(toggleGroup);
+
+        for (float i =DEFAULT_FONT_SIZE-3; i<DEFAULT_FONT_SIZE+5; i+=0.5)fontSize.getItems().add(i);
+        fontSize.setValue((float)DEFAULT_FONT_SIZE);
+        setContent(true, DEFAULT_FONT_SIZE);
 
 
     }
 
-    private String getContent() {
+    private void setContent(boolean freqs, float fs){
+        Platform.runLater(() -> webEngine.loadContent(getContent(freqs, fs)));
+    }
+
+    private String getContent(boolean freqs, float fontSize) {
 
         StringBuilder strB = new StringBuilder();
         strB.append("<html>");
         strB.append("<title></title>");
         strB.append("<head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/></head>");
-        strB.append("<body>");
+        strB.append("<body style='font-size:"+fontSize+"pt;'>");
 
         switch (type) {
 
             case 1:
-                profileContent(strB, id);
+                profileContent(strB, id, freqs, fontSize);
                 break;
 
             case 0:
-                complexContent(strB, id, false);
+                complexContent(strB, id, false, freqs, fontSize);
                 break;
             case 2:
-                ids.forEach(itm -> complexContent(strB, itm, true));
+                ids.forEach(itm -> complexContent(strB, itm, true, freqs, fontSize));
                 break;
         }
 
@@ -118,7 +134,7 @@ public class PrintController extends BaseController {
     }
 
 
-    private void complexContent(StringBuilder strb, long idComplex, boolean part) {
+    private void complexContent(StringBuilder strb, long idComplex, boolean part, boolean freqs, float fs) {
         TherapyComplex therapyComplex = getModel().findTherapyComplex(idComplex);
         if (getModel().countTherapyPrograms(therapyComplex, false) == 0) return;
         strb.append("<p></p>");
@@ -136,17 +152,23 @@ public class PrintController extends BaseController {
         strb.append("<p></p>");
 
         // strb.append("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\" style=\"max-width:600px;\">");
-        strb.append("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\" width=\"100%\">");
-        strb.append("<tr>");
-        strb.append("<th width=20%>" + res.getString("app.table.program_name") + "</th>");
-        strb.append("<th width=80%>" + res.getString("app.table.freqs") + "</th>");
-        strb.append("</tr>");
-
+        if(freqs){
+            strb.append("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\" width=\"100%\">");
+            strb.append("<tr>");
+            strb.append("<th width=20% >" + res.getString("app.table.program_name") + "</th>");
+            strb.append("<th width=80%>" + res.getString("app.table.freqs") + "</th>");
+            strb.append("</tr>");
+        }else {
+            strb.append("<table border=\"1\" cellspacing=\"0\" cellpadding=\"5\" width=\"50%\">");
+            strb.append("<tr>");
+            strb.append("<th><span style='font-size:"+fs+"pt;'>" + res.getString("app.table.program_name") + "</span></th>");
+            strb.append("</tr>");
+        }
         getModel().findTherapyPrograms(therapyComplex).forEach(itm ->
         {
             strb.append("<tr>");
-            strb.append("<td>" + itm.getName() + "</td>");
-            strb.append("<td>" + itm.getFrequencies().replace(";", "; ") + "</td>");
+            strb.append("<td><span style='font-size:"+fs+"pt;'>" + itm.getName() + "</span></td>");
+            if(freqs) strb.append("<td><span style='font-size:"+fs+"pt;'>" + itm.getFrequencies().replace(";", "; ") + "</span></td>");
             strb.append("</tr>");
         });
 
@@ -154,18 +176,33 @@ public class PrintController extends BaseController {
 
     }
 
-    private void profileContent(StringBuilder strb, long idProfile) {
+    private void profileContent(StringBuilder strb, long idProfile, boolean freqs, float fs) {
         Profile profile = getModel().findProfile(idProfile);
         strb.append("<p></p>");
         strb.append("<h1>" + res.getString("app.profile") + " - " + profile.getName() + "</h1>");
         strb.append("<p>" + res.getString("app.table.delay") + ": " + DateUtil.replaceTime(DateUtil.convertSecondsToHMmSs(getModel().getTimeProfile(profile)), res) + "</p>");
 
         strb.append("<p></p>");
-        getModel().findAllTherapyComplexByProfile(profile).forEach(itm -> complexContent(strb, itm.getId(), true));
+        getModel().findAllTherapyComplexByProfile(profile).forEach(itm -> complexContent(strb, itm.getId(), true, freqs, fs));
 
     }
 
+    public void choiceFreqs(){
+        setContent(true, fontSize.getValue());
+    }
+
+    public void choiceNoFreqs(){
+        setContent(false, fontSize.getValue());
+    }
+
+    public void choiceFontSize() {
+
+        setContent(freqRadio.isSelected(), fontSize.getValue());
+
+    }
 }
+
+
 
 /*
 
