154ab718b18cf5b5fb485d7a7eb79fbb412eab18
lightway82 <lightway82@gmail.com>
2020-11-13 18:53:26 +0300

    Обмен данными с js, основные функции рабты с сообщениями в js, базовый шаблон сообщений

--- mair_app/src/main/java/ru/biomedis/biomedismair3/social/contacts/lenta/MessagesService.kt
+++ mair_app/src/main/java/ru/biomedis/biomedismair3/social/contacts/lenta/MessagesService.kt
@@ -1,47 +1,77 @@
 package ru.biomedis.biomedismair3.social.contacts.lenta
 
 import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.ast.Node
 import com.vladsch.flexmark.util.data.MutableDataSet
 import javafx.scene.web.WebEngine
 import javafx.scene.web.WebView
-
-import com.vladsch.flexmark.util.ast.Node;
-
-import com.vladsch.flexmark.parser.Parser;
+import netscape.javascript.JSObject
+import java.util.*
 import kotlin.random.Random
 
 
 class MessagesService(val messagesArea: WebView, val messageEditorArea: WebView) {
     private lateinit var parser: Parser
     private lateinit var htmlrenderer: HtmlRenderer
-    private  var messagesEngine: WebEngine
-    private  var messageEditorEngine: WebEngine
-    val htmlMessagesPath: String =  "/html/chat.html"
+    private lateinit var messagesEngine: WebEngine
+    private lateinit var messageEditorEngine: WebEngine
+    val htmlMessagesPath: String = "/html/chat.html"
     val htmlEditorPath: String = "/html/msg_editor.html"
+    val javaConnector: JavaConnector
 
-     init {
-        fun initEditor(htmlPath: String, webView: WebView): WebEngine {
+    init {
+        javaConnector = JavaConnector(editAction = this::editMsgAction, deleteAction = this::deleteMsgAction)
+        fun initEditor(htmlPath: String, webView: WebView): Pair<WebEngine, JSObject> {
             val engine = webView.engine
             engine.isJavaScriptEnabled = true
+            val window: JSObject = engine.executeScript("window") as JSObject
             val html = javaClass.getResourceAsStream(htmlPath).bufferedReader().use { it.readText() }
             engine.loadContent(html)
-            return engine
+            return Pair(engine, window)
         }
-        messagesEngine = initEditor(htmlMessagesPath, messagesArea)
-        messageEditorEngine = initEditor(htmlEditorPath, messageEditorArea)
 
-         initMarkdownParser()
+        fun initMessaging() {
+            val initEditor = initEditor(htmlMessagesPath, messagesArea)
+            messagesEngine = initEditor.first
+            initEditor.second.setMember("javaConnector", javaConnector)
+            messagesEngine.documentProperty().addListener { _, oldValue, newValue ->
+                if (Objects.nonNull(newValue)) {
+                    val window = messagesEngine.executeScript("window") as JSObject
+                    window.setMember("javaConnector", javaConnector)
+                }
+            }
+
+        }
+
+        fun initEditing() {
+            messageEditorEngine = initEditor(htmlEditorPath, messageEditorArea).first
+        }
+
+        //
+
+        initMessaging()
+        initEditing()
+        initMarkdownParser()
 
     }
 
-    private fun initMarkdownParser(){
+    private fun editMsgAction(id: Long) {
+
+    }
+
+    private fun deleteMsgAction(id: Long) {
+
+    }
+
+    private fun initMarkdownParser() {
         val options = MutableDataSet()
         this.parser = Parser.builder(options).build()
         this.htmlrenderer = HtmlRenderer.builder(options).build()
     }
 
 
-    fun clear(){
+    fun clear() {
 
     }
     //TODO сделать стратегию кеширования, чтобы уже загруженные данные  сохранялись в памяти, чтобы при переключении можно было быстро вернуться
@@ -49,17 +79,17 @@ class MessagesService(val messagesArea: WebView, val messageEditorArea: WebView)
     /**
      * Установит данные контакта и загрузит сообщения
      */
-    fun showContact(contactId: Long){
+    fun showContact(contactId: Long) {
 
     }
 
     /**
      * Отправит введенное сообщение
      */
-    fun sendMessage(){
+    fun sendMessage() {
         var msg = getMessageFromEditor()
         var html = markdownToHtml(msg)
-        addMessage(html, 111)
+        addMessage(html, Random.nextLong(100))
 
     }
 
@@ -68,26 +98,40 @@ class MessagesService(val messagesArea: WebView, val messageEditorArea: WebView)
         return messageEditorEngine.executeScript("getMessageFromEditor()") as String
     }
 
-    private fun setEditorContent(mdContend: String){
+    private fun setEditorContent(mdContend: String) {
 
         messageEditorEngine.executeScript("setContent('${clearContent(mdContend)}')")
     }
 
-    private fun markdownToHtml(html: String): String{
+    private fun markdownToHtml(html: String): String {
         val document: Node = parser.parse(html)
-       return htmlrenderer.render(document)
+        return htmlrenderer.render(document)
     }
 
-    private fun addMessage(msg: String, msgId: Long){
+    private fun addMessage(msg: String, msgId: Long) {
         messagesEngine.executeScript("addMessage('${clearContent(msg)}', '$msgId')")
     }
 
-    private fun clearContent(c: String): String{
-       // var content = c.replace("'", "\\'")
-        var content  = c.replace(System.getProperty("line.separator"), "\\n")
+    /**
+     * Очистит строки от некорректных символов, экранирует кавычки иначе строку не передать в JS(кавычки сломают строку итп)
+     */
+    private fun clearContent(c: String): String {
+        var content = c.replace("'", "\\'")
+        content = content.replace("\"", "\\\"")
+        content = content.replace(System.getProperty("line.separator"), "\\n")
         content = content.replace("\n", "\\n")
         content = content.replace("\r", "\\n")
         return content
     }
 
+    class JavaConnector(val editAction: (Long) -> Unit, val deleteAction: (Long) -> Unit) {
+        fun deleteMsg(id: Long) {
+            deleteAction(id)
+        }
+
+        fun editMsg(id: Long) {
+            editAction(id)
+
+        }
+    }
 }
--- mair_app/src/main/resources/html/js/chat.js
+++ mair_app/src/main/resources/html/js/chat.js
@@ -4,16 +4,30 @@ $(document).ready(function () {
     $("#content").css("height", (window.innerHeight-20)+"px")
   });
 
+  $("#content").on("click","a.editBtn", function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    javaConnector.editMsg(parseInt($(this).data("msg_id")));
+   // updateMessage("111111111 ", $(this).data("msg_id"))
+  })
+
+  $("#content").on("click","a.deleteBtn", function (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    javaConnector.deleteMsg(parseInt($(this).data("msg_id")));
+    //removeMessage($(this).data("msg_id"))
+  })
 })
 
 
 
 function addMessage(htmlMsg, msgId){
-  $("#content").append("<div style='padding: 10px;' id='msg_id_"+msgId+"'>"+htmlMsg+"</div>")
+  $("#content table tbody")
+  .append(fillTemplate("msg_item_tpl", {htmlMsg:htmlMsg, msgId:msgId}))
  }
 
 function updateMessage(htmlMsg, msgId){
-  var msgContainer= $("#content div[id*='msg_id_'"+msgId+"]");
+  var msgContainer= $("#content tr[id*='msg_id_"+msgId+"'] td.msg_content");
   if(msgContainer) {
     msgContainer.empty().html(htmlMsg);
     return true;
@@ -21,13 +35,37 @@ function updateMessage(htmlMsg, msgId){
 }
 
 function removeMessage(msgId){
-  var msgContainer= $("#content div[id*='msg_id_'"+msgId+"]");
+  var msgContainer= $("#content tr[id*='msg_id_"+msgId+"']");
+  var spaceContainer= $("#content tr[data-msg_id_*='"+msgId+"']");
   if(msgContainer) {
     msgContainer.remove();
+    spaceContainer.remove();
     return true;
   }else return false;
 }
 
+
+/**
+ * Использует mustache. Переменные щаблона обрамляются {{ переменная }}
+ * @param templateID
+ * @param templateVars объект типа {var:val,..., } в качестве значения может быть функция
+ * @returns {void | string | *}
+ */
+function fillTemplate(templateID = "", templateVars = {}) {
+  let template = $('#' + templateID).text();
+  return Mustache.render(template, templateVars);
+}
+
+
+/**
+ * Использует mustache. Переменные щаблона обрамляются {{ переменная }}
+ * @param template строка шаблона
+ * @param templateVars объект типа {var:val,..., } в качестве значения может быть функция
+ * @returns {void | string | *}
+ */
+function fillTemplateString(template = "", templateVars = {}) {
+  return Mustache.render(template, templateVars);
+}
 
 
 
